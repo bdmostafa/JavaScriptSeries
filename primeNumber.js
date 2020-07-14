64aedae996725aef4b6e0ab2201a5ee2b4aacfb7
Mostafa <mdmostafaalmahmud@gmail.com>
2020-07-14 13:09:32 +0600

    update

--- primeNumber.js
+++ primeNumber.js
@@ -1,11 +1,17 @@
 function isPrime(n) {
-    for (let i = 2; i < n / 2; i++) {
-        if (n % i == 0) {
-            return `${n} is not a prime number`;
+    if (n === 1) {
+        return `${n} is not a prime number`;
+    } else if (n === 2) {
+        return `${n} is a prime number`;
+    } else {
+        for (let i = 2; i < n / 2; i++) {
+            if (n % i == 0) {
+                return `${n} is not a prime number`;
+            }
         }
+        return `${n} is a prime number`;
     }
-    return `${n} is a prime number`;
 }
 
-const prime = isPrime(13);
+const prime = isPrime(15);
 console.log(prime)
