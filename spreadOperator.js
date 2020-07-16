83f622a0d3de87c9536bf9250441b40fa8e9545c
Mostafa <mdmostafaalmahmud@gmail.com>
2020-07-16 10:44:59 +0600

    spread operator in array and object

---
+++ spreadOperator.js
@@ -0,0 +1,28 @@
+// Spread usages on array and object for copying arr and obj
+
+// Spread on array ===================================================
+const arrOne = ['I', 'love', 'Bangladesh']
+const arrTwo = ['Dhaka', 'is', 'a', 'capital', 'of', 'BD']
+
+// const arrThree = arrOne.concat(arrTwo);
+
+// Use ... spread operator to flat the array [[ , , ], [ , , ]] ===> [, , , , ]
+const arrThree = [...arrOne, 'and', ...arrTwo]
+console.log(arrThree);
+
+// Spread on object =========================================
+const person = {
+    name: 'Mostafa',
+    age: 30,
+    gender: 'male',
+    status: 'developer'
+}
+
+const updatePerson = {
+    // person,
+    // spread to flat the object { { }, { }} ===> { }
+    ...person,
+    degree: 'MSC'
+}
+
+console.log(updatePerson)
