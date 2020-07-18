4931af133d40031a39aad0b92697521b490c79c2
Mostafa <mdmostafaalmahmud@gmail.com>
2020-07-18 20:43:51 +0600

    scope

--- scope.js
+++ scope.js
@@ -1,8 +1,8 @@
-// local scope - scope chain (check low to high)
-// global scope - variable assign globally
+// {local scope} - scope chain (check low to high) -> functional scope, block scope - only accessible within the function and child function
+// global scope { ... } - variable assign globally
 let a = 3;
 
-// b and c are n/A outside of function
+// b and c are n/A outside of function (local scope)
 // console.log(b);
 // console.log(c);
 
@@ -22,11 +22,15 @@ function outerFunction() {
 }
 
 {
+    // block scope
     const a = 11;
+    const b = 13
     console.log(a); // 11
+    console.log(b); // 13
 }
 
 console.log(a); // 3
+// console.log(b); (n/A)
 
 
 // call outerFunction
