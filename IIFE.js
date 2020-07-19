90a4c26e0ea74ee02a0db7565652c790e986b8b6
Mostafa <mdmostafaalmahmud@gmail.com>
2020-07-19 10:31:38 +0600

    IIFE updated

--- IIFE.js
+++ IIFE.js
@@ -1,6 +1,7 @@
 // An IIFE (Immediately Invoked Function Expression) is a JavaScript function that runs as soon as it is defined.
 // It is not recommended to declare a variable globally if it is not important because it can override others file when rendering. So to be safe it is to be inside IIFE so that it is under local scope
 
+// function itself runs and returns output as an expression, not statement
 (function () {
     let persons = [{
             name: 'Mostafa',
@@ -43,15 +44,71 @@
             } = person;
 
             singlePerson += `
-        Name: ${name}
-        Age: ${age}
-        Gender: ${gender}
-        Status: ${status}
-        ${availability ? 'Available to work' : 'Not available'}
-        `
+                Name: ${name}
+                Age: ${age}
+                Gender: ${gender}
+                Status: ${status}
+                ${availability ? 'Available to work' : 'Not available'}
+            `
         }
         return singlePerson;
     }
     const person = showPersonInfo(persons);
     console.log(person);
 }())
+
+// function assigned to a variable and returns output as an expression, not statement
+const person = (function () {
+    let persons = [{
+            name: 'Mostafa',
+            age: 29,
+            gender: 'male',
+            status: 'developer',
+            availability: true
+        },
+        {
+            name: 'Mahmud',
+            age: 32,
+            gender: 'male',
+            status: 'designer',
+            availability: false
+        }
+    ];
+
+    function showPersonInfo(persons) {
+        let singlePerson = '';
+
+        for (let person of persons) {
+
+            singlePerson += `
+            Name: ${person.name}
+            Age: ${person.age}
+            Gender: ${person.gender}
+            Status: ${person.status}
+            ${person.availability ? 'Available to work' : 'Not available'}
+            `
+
+            // Or we can do obj destructuring to minimize coding
+            // const {
+            //     name,
+            //     age,
+            //     gender,
+            //     status,
+            //     availability
+            // } = person;
+
+            // singlePerson += `
+            //     Name: ${person.name}
+            //     Age: ${person.age}
+            //     Gender: ${person.gender}
+            //     Status: ${person.status}
+            //     ${person.availability ? 'Available to work' : 'Not available'}
+            // `
+        }
+        return singlePerson;
+    }
+    const person = showPersonInfo(persons);
+    return person;
+}())
+// The same const 'person' here both inside and outside but it does not conflict with others because of local scope
+console.log(person);
