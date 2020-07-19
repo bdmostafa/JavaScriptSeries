948e1b323c4ae746dae1c439432dc27526ffa826
Mostafa <mdmostafaalmahmud@gmail.com>
2020-07-19 12:56:44 +0600

    Operator Precedence and Associativity

---
+++ operatorPrecedence.js.js
@@ -0,0 +1,8 @@
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Operator_Precedence
+
+// Operator precedence and associativity
+console.log(5 + 5);
+console.log(5 + 5 * 10 / 5);
+console.log((5 + 5) * 10 / 5);
+console.log(5 + 5 * (10 / 5));
+console.log((5 + (5 * 10)) / 5);
