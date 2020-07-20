f512ac8587006d13c265726f49b2b2da12b2affc
Mostafa <mdmostafaalmahmud@gmail.com>
2020-07-20 16:45:21 +0600

    constructor function

--- factoryVsConstructor.js
+++ factoryVsConstructor.js
@@ -1,5 +1,5 @@
 // An object
-const product = {
+const productObj = {
     productName: 'Mango',
     productPrice: 100,
     productQuantity: 4,
@@ -25,3 +25,37 @@ function createProductObj(productName, productPrice, productQuantity, productAva
 }
 
 console.log(createProductObj('Mango', 100, 4, true))
+
+
+// Constructor function - (blueprint / structure)
+// It is recommended to start with capital latter
+function Product(productName, productPrice, productQuantity, productAvailability) {
+    // return {
+    //     productName,
+    //     productPrice,
+    //     productQuantity,
+    //     productAvailability
+    // }
+
+    // 'this' KW indicates object 'product' that is created by calling constructor function
+    this.productName = productName
+    this.productPrice = productPrice
+    this.productQuantity = productQuantity
+    this.productAvailability = productAvailability
+    this.showProductInfo = function () {
+        return `Product Name - ${this.productName}, Price - ${this.productPrice}, Quantity - ${this.productQuantity}, Available - ${this.productAvailability}`
+    }
+    // Using with aero () function, without 'this' kw and value from function parameter directly
+    // this.showProductInfo = () => {
+    //     return `Product Name - ${productName}, Price - ${productPrice}, Quantity - ${productQuantity}, Available - ${productAvailability}`
+    // }
+
+}
+
+const product = new Product('Banana', 30, 12, false)
+console.log(product);
+console.log(product.productName);
+console.log(product.showProductInfo());
+
+const product2 = new Product('Onion', 45, '1 kg', true);
+console.log(product2.showProductInfo());
