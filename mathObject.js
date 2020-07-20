cdd7f65ec16255e2ea4f3e326f0770569cc77f18
Mostafa <mdmostafaalmahmud@gmail.com>
2020-07-20 21:34:44 +0600

    math object

---
+++ mathObject.js
@@ -0,0 +1,13 @@
+console.log(Math.random());
+console.log(Math.floor(4.99999));
+console.log(Math.round(5.49999999));
+console.log(Math.round(5.511111111111));
+console.log(Math.ceil(5.0000000001));
+console.log(Math.abs(-9.1));
+
+// In practice ================
+// Generate a random number (0 - 10)
+console.log(Math.floor(Math.random() * 11));
+
+// Generate a random number (0 - 11)
+console.log(Math.ceil(Math.random() * 11));
