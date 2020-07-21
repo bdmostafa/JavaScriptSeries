7fd142480b6aef4605f38f1c491a09927511e6ea
Mostafa <mdmostafaalmahmud@gmail.com>
2020-07-21 12:25:43 +0600

    this usages updated

--- this.js
+++ this.js
@@ -1,6 +1,8 @@
 // 1. global scope when 'this' is outside curly brackets and this indicates the whole window itself
 console.log(this); // {}
-
+this.sayHi = 'Hello';
+console.log(this); // {sayHi: 'Hello'}
+// It's not recommended to use in global scope (bad practice)
 
 // 2. 'this' indicates parent object when it is declared inside method
 // Here parent object is student
@@ -36,3 +38,47 @@ console.log(product1.showProductInfo());
 
 const product2 = new Product('Onion', 45, '1 kg', true);
 console.log(product2.showProductInfo());
+
+
+// 4. 'this' indicates window object in not strict mode but undefined in  in strict mode (local scope)
+// source: https://www.youtube.com/watch?v=saB8IvU3ojE&list=PLyrs5AgsUPcXhDGQ02G13cdp5Ts7jHR5k&index=60
+
+function sampleFunction() {
+    console.log(this);
+}
+sampleFunction();
+
+// Here 'this' indicates parent object student (from above)
+console.log(student.showStudentInfo());
+
+// Here 'this' indicates the window object because 'showStudentInfo' is called in a plain way without any reference
+const demoStudent = student.showStudentInfo;
+console.log(demoStudent()); // undefined because node js does not support window obj
+
+// So 'this' behaving relies on how we call the method whether with reference or in a plain way
+
+// To solve this problem use .call() method
+console.log(demoStudent.call(student));
+
+// ANother example of .call()
+const product = {
+    pName: 'Banana',
+    pPrice: 25,
+    pQuantity: 4,
+    showProductInfo() {
+        function innerFunction() {
+            // this indicates window object if .call() is not added in function call
+            console.log(this);
+        }
+        // innerFunction();
+        innerFunction.call(product);
+
+        console.log(this);
+        return `${this.pName} - ${this.pPrice} - ${this.pQuantity}`
+    }
+}
+
+console.log(product.showProductInfo());
+const demoProduct = product.showProductInfo;
+console.log(demoProduct());
+console.log(demoProduct.call(product));
