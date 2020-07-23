6a42befd7a74a958ebf2ab27e0a4d253d82300ab
Mostafa <mdmostafaalmahmud@gmail.com>
2020-07-23 12:38:25 +0600

    removing last element child - DOM

--- DOM/manipulateElement.js
+++ DOM/manipulateElement.js
@@ -17,7 +17,19 @@ newHeading.appendChild(document.createTextNode('Product List (updated)'));
 // const parentHeading = document.querySelector('.container');
 // parentHeading.replaceChild(newHeading, preHeading);
 
-// Without parentHeading - .replaceWith method
+// Or without parentHeading - .replaceWith method
 preHeading.replaceWith(newHeading)
 
-console.log(newHeading)
+console.log(newHeading);
+
+
+// Removing last element child ========================
+// const list = document.querySelector('.product-collection');
+// list.lastElementChild.remove();
+
+// Another way to remove last child with parent
+const list = document.querySelector('ul.product-collection');
+list.removeChild(list.lastElementChild)
+
+
+console.log(list)
