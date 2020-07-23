7a4c85bd570fc8b8962087d02ffe4cd5982db409
Mostafa <mdmostafaalmahmud@gmail.com>
2020-07-23 16:08:39 +0600

    bubbling and delegation - DOM

---
+++ DOM/bubbling&Delegation.js
@@ -0,0 +1,25 @@
+// Event Bubbling (child to parent)
+const li = document.querySelector('li');
+const ul = document.querySelector('ul');
+const container = document.querySelector('.container');
+
+li.addEventListener('click', () => {
+    console.log('clicked li')
+})
+
+ul.addEventListener('click', () => {
+    console.log('clicked ul')
+})
+
+container.addEventListener('click', () => {
+    console.log('clicked container')
+})
+
+// Event Delegation (parent to child)
+// If any element under container, it executes
+container.addEventListener('dblclick', event => {
+    // Any other element does not effect without target id
+    if (event.target.id === 'delegationSample') {
+        event.target.classList.add('delegationSample');
+    }
+})
