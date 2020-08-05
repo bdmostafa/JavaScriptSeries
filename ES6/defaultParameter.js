ac25396a24f2fc89b80a429bb7514df5b24a86b5
Mostafa <mdmostafaalmahmud@gmail.com>
2020-08-05 07:58:19 +0600

    default parameter in function statement and function expression

---
+++ ES6/defaultParameter.js
@@ -0,0 +1,23 @@
+// Default parameter / default function arguments
+// Function statement
+function multiply(num1 = 0, num2 = 2) {
+    return num1 * num2;
+}
+
+console.log(multiply(2, 3)); // 6
+console.log(multiply(2)); // 4
+console.log(multiply(undefined, 3)); // 0
+console.log(multiply(2, undefined)); // 4
+
+// Function expression
+const add = function (num1 = 0, num2 = 2) {
+    return num1 + num2;
+}
+console.log(add(undefined, 1)); // 1
+console.log(add(1)); // 3
+
+// NaN because not to compute with undefined (undefined + 2) = NaN
+const add2 = function (num1, num2 = 2) {
+    return num1 + num2;
+}
+console.log(add2(undefined, 1)); // NaN
