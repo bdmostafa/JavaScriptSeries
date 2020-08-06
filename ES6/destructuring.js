520ab0fed1f5146f0a888e35778372ad44297edb
Mostafa <mdmostafaalmahmud@gmail.com>
2020-08-06 13:06:09 +0600

    destructuring updated

--- ES6/destructuring.js
+++ ES6/destructuring.js
@@ -6,11 +6,17 @@ const myBio = ['Mostafa', 29, 'Web Developer'];
 // const [, age] = myBio;
 // const [, , profession] = myBio;
 
-const [name, age, profession] = myBio;
+const [name, age, profession] = myBio; // Index-variable order maintain must be followed in array destructuring
 
 console.log(name); // Mostafa
 console.log(age); // 29
 console.log(profession); // Web Developer
+
+const friendsArray = ['a', 'b', 'c', 'd', 'e']
+const [bestFriend, ...averageFriends] = friendsArray;
+
+console.log(bestFriend); // a
+console.log(averageFriends); // [ 'b', 'c', 'd', 'e' ] 
 
 // Object destructuring
 const objBio = {
@@ -36,6 +42,7 @@ const {
 console.log(userName, id, status, isMarried);
 // Mostafa 1 Developer true 
 
+// Complex object destructuring (objBio.shareBio)
 const {
     facebook,
     instagram
