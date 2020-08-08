195cca27ad0f39e16a2cb4d076ea79e00e0ff825
Mostafa <mdmostafaalmahmud@gmail.com>
2020-08-08 08:30:05 +0600

    fetch API - json placeholder data display

--- ES6/fetchAPI.js
+++ ES6/fetchAPI.js
@@ -1,5 +1,26 @@
 // fetch API - works with Promise
+/*
 fetch('https://jsonplaceholder.typicode.com/todos/1')
     .then(response => response.json()) // Object
     .then(json => console.log(json)) // data
     .catch(error => console.log(error)); // For error handling
+ */
+
+fetch('https://jsonplaceholder.typicode.com/users')
+    .then(response => response.json()) // Object
+    .then(json => displayUserData(json)) // data
+    .catch(error => console.log(error)); // For error handling
+
+// In practice
+function displayUserData(users) {
+    const ul = document.getElementById('users-container');
+    const userNames = users.map(user => user.username);
+
+    for (let i = 0; i < userNames.length; i++) {
+        const username = userNames[i];
+        const li = document.createElement('li');
+        li.innerText = username;
+        li.style.listStyle = 'none';
+        ul.appendChild(li);
+    }
+}
