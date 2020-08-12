2805a1eb420ee5d201741c8203282093f4770fe4
Mostafa <mdmostafaalmahmud@gmail.com>
2020-08-12 14:32:01 +0600

    async/await

--- advance-js/asyncAwait.js
+++ advance-js/asyncAwait.js
@@ -9,11 +9,16 @@ loadData().then(data => displayUserData(data));
 function displayUserData(data) {
     const ul = document.querySelector('ul');
     const userNames = data.map(user => user.username);
+    const userEmails = data.map(user => user.email);
 
     for (let i = 0; i < userNames.length; i++) {
-        const username = userNames[i];
+        const userName = userNames[i];
+        const userEmail = userEmails[i];
         const li = document.createElement('li');
-        li.innerText = username;
+        const p = document.createElement('p');
+        li.innerText = userName;
+        p.innerText = userEmail;
+        li.appendChild(p)
         ul.appendChild(li);
     }
 }
