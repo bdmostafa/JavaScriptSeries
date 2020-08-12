d3caff66927a8bc487469d7bcd6155064f10844b
Mostafa <mdmostafaalmahmud@gmail.com>
2020-08-12 18:43:04 +0600

    another example of async/await added

--- advance-js/Asynchronous.js
+++ advance-js/Asynchronous.js
@@ -54,40 +54,40 @@ function getNum(num, callback) {
 // 2. course enrollment - 2s
 // 3. price of the course - 2s
 
-// getUser(1, function (user) {
-//     console.log(user);
-//     enrolledCourses(user.name, function (courses) {
-//         console.log(courses);
-//         getPrice(courses[0], function () {
-//             console.log('JavaScript course price is 500 USD.')
-//         })
-//     })
-// })
+getUser(1, function (user) {
+    console.log(user);
+    enrolledCourses(user.name, function (courses) {
+        console.log(courses);
+        getPrice(courses[0], function () {
+            console.log('JavaScript course price is 500 USD.')
+        })
+    })
+})
 
-// // Getting user - 2s
-// function getUser(id, callback) {
-//     setTimeout(() => {
-//         callback({
-//             id: 1,
-//             name: 'Mostafa',
-//             profession: 'Web Developer'
-//         });
-//     }, 2000)
-// }
+// Getting user - 2s
+function getUser(id, callback) {
+    setTimeout(() => {
+        callback({
+            id: 1,
+            name: 'Mostafa',
+            profession: 'Web Developer'
+        });
+    }, 2000)
+}
 
-// // course enrollment - 2s
-// function enrolledCourses(userName, callback) {
-//     setTimeout(() => {
-//         callback(['JavaScript', 'React', 'Node']);
-//     }, 2000)
-// }
+// course enrollment - 2s
+function enrolledCourses(userName, callback) {
+    setTimeout(() => {
+        callback(['JavaScript', 'React', 'Node']);
+    }, 2000)
+}
 
-// // price of the course - 2s
-// function getPrice(courseName, callback) {
-//     setTimeout(() => {
-//         callback()
-//     }, 2000)
-// }
+// price of the course - 2s
+function getPrice(courseName, callback) {
+    setTimeout(() => {
+        callback()
+    }, 2000)
+}
 
 // Promise (constructor function) =====================================================
 // Pending, resolved, reject
@@ -208,29 +208,29 @@ Promise.race([p1,
 
 
 
+// Another example of async/await ===============================
 
+async function loadData() {
+    const response = await fetch('https://jsonplaceholder.typicode.com/users')
+    const data = await response.json();
+    return data;
+}
 
-// async function loadData() {
-//     const response = await fetch('https://jsonplaceholder.typicode.com/users')
-//     const data = await response.json();
-//     return data;
-// }
+loadData().then(data => displayUserData(data));
 
-// loadData().then(data => displayUserData(data));
+function displayUserData(data) {
+    const ul = document.querySelector('ul');
+    const userNames = data.map(user => user.username);
+    const userEmails = data.map(user => user.email);
 
-// function displayUserData(data) {
-//     const ul = document.querySelector('ul');
-//     const userNames = data.map(user => user.username);
-//     const userEmails = data.map(user => user.email);
-
-//     for (let i = 0; i < userNames.length; i++) {
-//         const userName = userNames[i];
-//         const userEmail = userEmails[i];
-//         const li = document.createElement('li');
-//         const p = document.createElement('p');
-//         li.innerText = userName;
-//         p.innerText = userEmail;
-//         li.appendChild(p)
-//         ul.appendChild(li);
-//     }
-// }
+    for (let i = 0; i < userNames.length; i++) {
+        const userName = userNames[i];
+        const userEmail = userEmails[i];
+        const li = document.createElement('li');
+        const p = document.createElement('p');
+        li.innerText = userName;
+        p.innerText = userEmail;
+        li.appendChild(p)
+        ul.appendChild(li);
+    }
+}
