5917e51f58ce59d09285f99489723f5b3cb31267
Mostafa <mdmostafaalmahmud@gmail.com>
2020-08-13 08:43:08 +0600

    AJAX-JSON in practice

--- AJAX.JSON.API/main.js
+++ AJAX.JSON.API/main.js
@@ -12,11 +12,24 @@ function loadData() {
         if (this.readyState === 4 && this.status === 200) {
             // console.log('everything is ready');
             // console.log(xhr.responseText);
-            document.getElementById('data').textContent = this.responseText;
+            document.getElementById('data').textContent = JSON.parse(this.responseText);
+            console.log(this.responseText); // as json data
+            const data = JSON.parse(this.responseText);
+            console.log(data); // as string object
+
+            let displayData = [];
+            for (let i = 0; i < data.length; i++) {
+                displayData.push(`Name: ${data[i].name},
+                Age: ${data[i].age},
+                Profession: ${data[i].profession}`)
+
+            };
+            document.getElementById('data').textContent = displayData;
         }
     }
 
     // xhr.open('GET', 'https://jsonplaceholder.typicode.com/todos/', true);
-    xhr.open('GET', './ajax.txt', true);
+    // xhr.open('GET', './ajax.txt', true);
+    xhr.open('GET', './sample.json', true)
     xhr.send();
 }
