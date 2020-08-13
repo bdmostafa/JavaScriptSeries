a90dac8a47e3e673586a1bb2f70c3357b64629b3
Mostafa <mdmostafaalmahmud@gmail.com>
2020-08-13 08:15:21 +0600

    AJAX shortcut way- XMLHttpRequest constructor- xhr.open/.send practice

--- AJAX.JSON.API/main.js
+++ AJAX.JSON.API/main.js
@@ -2,19 +2,21 @@ const btn = document.getElementById('btn');
 btn.addEventListener('click', loadData);
 
 function loadData() {
-    const xhttp = new XMLHttpRequest();
-    xhttp.onreadystatechange = function () {
+    // Instantiate constructor function
+    const xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function () {
         // console.log('state', this.readyState);
         // console.log('status', this.status);
 
+        // To check if everything is okay
         if (this.readyState === 4 && this.status === 200) {
             // console.log('everything is ready');
-            // console.log(xhttp.responseText);
+            // console.log(xhr.responseText);
             document.getElementById('data').textContent = this.responseText;
         }
     }
 
-
-    xhttp.open('GET', './ajax.txt', true);
-    xhttp.send();
+    // xhr.open('GET', 'https://jsonplaceholder.typicode.com/todos/', true);
+    xhr.open('GET', './ajax.txt', true);
+    xhr.send();
 }
